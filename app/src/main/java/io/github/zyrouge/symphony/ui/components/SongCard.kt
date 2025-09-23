a835e6ea8e47c7b7636baee5c60beb6c31e3bf0c
imgde <47184194+imgde@users.noreply.github.com>
2025-09-23 11:51:52 +0200

    fix state issue

--- app/src/main/java/io/github/zyrouge/symphony/ui/components/SongCard.kt
+++ app/src/main/java/io/github/zyrouge/symphony/ui/components/SongCard.kt
@@ -207,7 +207,7 @@ fun SongDropdownMenu(
     expanded: Boolean,
     onDismissRequest: () -> Unit,
 ) {
-    val data by remember {
+    val data by remember(song, isFavorite, expanded) {
         mutableStateOf(
             SongDropdownMenuData(
                 context,
